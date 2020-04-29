c1ca4ff07e87395fad03bbb8d735fb19dc8d11fa
Arta Razavi <artarazavi@gmail.com>
2020-04-28 17:13:25 -0700

    calling is functional added in functionality from react-native-phone-call

--- App.js
+++ App.js
@@ -2,21 +2,49 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { db } from './config';
 import * as Font from 'expo-font';
+import call from 'react-native-phone-call';
 
 export default class App extends React.Component {
   state = {
     number: '',
+    eventStatus: '',
     fontsLoaded: false,
   }
 
+  call = (num) => {
+    const args = {
+      number: num,
+      prompt: false,
+    };
+    call(args).catch(console.error);
+  };
+
   componentDidMount= async()  => {
+    //load fonts
     await Font.loadAsync({'Audiowide': require('./assets/fonts/Audiowide-Regular.ttf')});
     this.setState({ fontsLoaded: true });
+
+    //get status
+    db.ref('test/event_status').on('value', snapshot => {
+      let data = snapshot.val();
+      this.setState({ eventStatus: data });
+    });
+
+    //get button press
     db.ref('test/button_press').on('value', snapshot => {
       let data = snapshot.val();
       this.setState({ number: data });
     });
   }
+
+  componentDidUpdate(){
+    //initiate call
+    if(this.state.eventStatus === "in call" && this.state.number.length > 0){
+      this.call(this.state.number);
+      db.ref('/test').set({ event_status:  "Ready for next call", button_press:  ""});
+    }
+  }
+
   render() {
     if(this.state.fontsLoaded) {
       return (
@@ -26,7 +54,7 @@ export default class App extends React.Component {
             source={require('./assets/images/hamburger.png')}
             style={styles.burgerImg}
           />
-          <Text>Calling</Text>
+          <Text>{this.state.eventStatus}</Text>
           <Text>{this.state.number}</Text>
         </View>
       )
@@ -45,7 +73,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   titleTxt: {
-    fontSize: 38,
+    fontSize: 36,
     fontFamily: 'Audiowide'
   },
   burgerImg: {
